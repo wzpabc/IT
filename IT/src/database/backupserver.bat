374f6b06ddd2e22e5df5f02911d2978daeac4916
upw023 <upw023@192.168.56.1>
2017-05-26 11:08:41 +0800

    消费

--- IT/src/database/backupserver.bat
+++ IT/src/database/backupserver.bat
@@ -1,6 +1,8 @@
+
+
 echo off
 echo %date:~8,2%-%time:~0,2%%time:~3,2%%time:~6,2% 
- mysqldump -uroot -proot it > "%cd%\it%date:~8,2%-%time:~0,2%%time:~3,2%%time:~6,2%.sql"
+sqlcmd -E -S.\sqlexpress -ibackupsqlserver.sql -vdirpath="C:\Users\upw023\git\IT\IT\src\database" -W
 echo "%cd%\it%date:~8,2%-%time:~0,2%%time:~3,2%%time:~6,2%.sql"
 rem sqlcmd -E -S.\sqlexpress -ibackupsqlserver.sql -vdirpath=%cd% -W
 exit
